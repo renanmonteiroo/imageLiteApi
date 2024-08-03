777c46543201bc042d4d63ef18b2ee9f94624059
renan monteiro <renan.batista@sad.pe.gov.br>
2024-08-03 15:22:08 -0300

    Criação de projeto e criação de imagesController

---
+++ imageliteapi/src/test/java/io/github/renanmonteiroo/imageliteapi/ImageliteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.renanmonteiroo.imageliteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageliteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
